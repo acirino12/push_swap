4b40081a582ba2804ec1157d6c4274bf7ee64632
acirino12 <an_cirin@hotmail.it>
2026-06-22 15:57:29 +0200

    Introduce strategy variable and initial comments
    Added strategy variable and comments for strategy selection logic.

--- main.c
+++ main.c
@@ -2,6 +2,9 @@
 
 int main(int argc, char **argv)
 {
+	int	strategy;
+
+	strategy = 0;
 	// Controlla: argc > 1. 
 	// Caso 1> ./push_swap "numeri"
 	// Caso 2> ./push_swap n0 n1 n2 n3 ...
@@ -10,13 +13,15 @@ int main(int argc, char **argv)
 	{
 
 	}
-	// Parsing su?
+	// Parsing chiamo
 	
 	// Qui abbiamo creato la lista. Dobbiamo chiamare una funzione che assegni gli indici.
 	(void) assign_indexes(t_stack *a)
 	
 	// Calcoliamo il disordine compute_disorder=  mistakes / total_pairs
 	
+	// Funzione che se stragy e' 0 cambia strategy: 1(simple) < 20 percento, 2(medium) tra >= 20 e <= 65, 3(complex) sopra > 65
+		
 	// Stampiamo i passi dell'algoritmo scelto 
 		
 }
