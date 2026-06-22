03f12fa6f8d230977dd3c947ee0481037e4410ba
acirino12 <an_cirin@hotmail.it>
2026-06-22 14:45:54 +0200

    Create main function for push_swap
    Initialize main function for push_swap program with comments and function calls.

---
+++ main.c
@@ -0,0 +1,22 @@
+#include "push_swap.h"
+
+int main(int argc, char **argv)
+{
+	// Controlla: argc > 1. 
+	// Caso 1> ./push_swap "numeri"
+	// Caso 2> ./push_swap n0 n1 n2 n3 ...
+	// Caso 1bis, 2bis> ./push_swap --strategy_selector (adaptive(caso base), simple, medium, complex)
+	if ()
+	{
+
+	}
+	// Parsing su?
+	
+	// Qui abbiamo creato la lista. Dobbiamo chiamare una funzione che assegni gli indici.
+	(void) assign_indexes(t_stack *a)
+	
+	// Calcoliamo il disordine compute_disorder=  mistakes / total_pairs
+	
+	// Stampiamo i passi dell'algoritmo scelto 
+		
+}
