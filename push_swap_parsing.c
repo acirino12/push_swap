fb21cd9b51f1131413ab96fb5e8b86f80d6e6753
MarcoCoppola135 <marcocoppola135@gmail.com>
2026-06-24 15:15:45 +0200

    Update push_swap_parsing.c

--- push_swap_parsing.c
+++ push_swap_parsing.c
@@ -1,163 +1,175 @@
 #include "push_swap.h"
 #include <limits.h>
 
-// Ritorna 1 in caso di successo, 0 in caso di errore (caratteri errati o overflow)
-int ft_atoi_save(const char *str, int *result)
+/**
+ * Libera completamente la matrice di stringhe allocata da ft_split
+ */
+void	free_matrix(char **matrix)
 {
-    long    num;
-    int     sign;
-    int     i;
+	int	i;
 
-    num = 0;
-    sign = 1;
-    i = 0;
-    if (str[i] == '-' || str[i] == '+')
-    {
-        if (str[i] == '-')
-            sign = -1;
-        i++;
-    }
-    if (!str[i]) // Stringa vuota o solo un segno "+" o "-"
-        return (0);
-    while (str[i])
-    {
-        if (str[i] < '0' || str[i] > '10') // Verifica caratteri validi
-            return (0);
-        num = num * 10 + (str[i] - '0');
-        // Controllo overflow immediato durante il ciclo
-        if ((sign == 1 && num > INT_MAX) || (sign == -1 && (-num) < INT_MIN))
-            return (0);
-        i++;
-    }
-    *result = (int)(num * sign);
-    return (1);
-}
-
-int has_duplicate(t_stack *stack_a, int value)
-{
-    t_node  *current;
-
-    if (!stack_a || !stack_a->top)
-        return (0);
-    current = stack_a->top;
-    while (current != NULL)
-    {
-        if (current->value == value)
-            return (1); // Duplicato trovato
-        current = current->next;
-    }
-    return (0);
-}
-
-int push_back(t_stack *stack_a, int value)
-{
-    t_node  *new_node;
-    t_node  *last;
-
-    new_node = malloc(sizeof(t_node));
-    if (!new_node)
-        return (0);
-    new_node->value = value;
-    new_node->index = -1; // L'indice verrà calcolato successivamente
-    new_node->next = NULL;
-    if (!stack_a->top)
-    {
-        new_node->prev = NULL;
-        stack_a->top = new_node;
-    }
-    else
-    {
-        last = stack_a->top;
-        while (last->next)
-            last = last->next;
-        last->next = new_node;
-        new_node->prev = last;
-    }
-    stack_a->size++;
-    return (1);
-}
-
-void free_stack(t_stack *stack)
-{
-    t_node  *current;
-    t_node  *next_node;
-
-    if (!stack)
-        return ;
-    current = stack->top;
-    while (current)
-    {
-        next_node = current->next;
-        free(current);
-        current = next_node;
-    }
-    stack->top = NULL;
-    stack->size = 0;
-}
-
-// Cambia string comparison
-
-int    strategy_selector(char *s, int *strategy)
-{
-	if(strcmp(s, "adaptive"))
-		return (1);
-	if(strcmp(s, "simple"))
+	if (!matrix)
+		return ;
+	i = 0;
+	while (matrix[i])
 	{
-		*strategy = 1;
-		return (1);
+		free(matrix[i]);
+		i++;
 	}
-	if(strcmp(s, "medium"))
+	free(matrix);
+}
+
+/**
+ * Gestisce l'errore di parsing stampando "Error" su stderr,
+ * liberando la memoria attiva e terminando il programma.
+ */
+static void	handle_parse_error(t_pushswap *ps, char **matrix)
+{
+	write(2, "Error\n", 6);
+	if (matrix)
+		free_matrix(matrix);
+	free_stack(&ps->a);
+	exit(1);
+}
+
+/**
+ * Converte una stringa in un intero controllando la presenza di overflow.
+ * Ritorna 1 in caso di successo, 0 se la stringa contiene caratteri errati o sfora INT.
+ */
+int	ft_atoi_save(const char *str, int *result)
+{
+	long	num;
+	int		sign;
+	int		i;
+
+	num = 0;
+	sign = 1;
+	i = 0;
+	while (str[i] == ' ' || (str[i] >= 9 && str[i] <= 13))
+		i++;
+	if (str[i] == '-' || str[i] == '+')
 	{
-		*strategy = 1;
-		return (2);
+		if (str[i] == '-')
+			sign = -1;
+		i++;
 	}
-	if(strcmp(s, "complex"))
+	if (str[i] < '0' || str[i] > '9')
+		return (0);
+	while (str[i] >= '0' && str[i] <= '9')
 	{
-		*strategy = 1;
-		return (3);
+		num = num * 10 + (str[i] - '0');
+		if ((sign == 1 && num > INT_MAX) || (sign == -1 && (-num) < INT_MIN))
+			return (0);
+		i++;
+	}
+	if (str[i] != '\0')
+		return (0);
+	*result = (int)(num * sign);
+	return (1);
+}
+
+/**
+ * Verifica se un valore è già presente all'interno dello stack A.
+ * Ritorna 1 se duplicato, 0 altrimenti.
+ */
+int	has_duplicate(t_stack *stack_a, int value)
+{
+	t_node	*current;
+
+	if (!stack_a || !stack_a->top)
+		return (0);
+	current = stack_a->top;
+	while (current != NULL)
+	{
+		if (current->value == value)
+			return (1);
+		current = current->next;
 	}
 	return (0);
 }
 
-// Ricorda di modificare per gestire l'aggiunta di ft_split nel caso in cui i numeri ricevuti in argomento siano racchiusi da virgolette (es.: "1 2 3")
-
-int parse_arguments(int argc, char **argv, t_stack *stack_a, int *strategy)
+/**
+ * Alloca e inserisce un nuovo nodo in coda allo stack A.
+ * Ritorna 1 in caso di successo, 0 se la malloc fallisce.
+ */
+int	push_back(t_stack *stack_a, int value)
 {
-	int	i;
-    int	value;
+	t_node	*new_node;
+	t_node	*last;
 
-    i = 1; // Partiamo da 1 per saltare il nome dell'eseguibile
-    strategy = 0;
-    
-    while (i < argc)
-    {
-        if (1 == i && argv[1][1] == '-' && argv[1][2] == '-')
-        {
-            if(strategy_selector(argv[1] + 2, strategy))
-                i++;
-        }
-        // 1. Controlla caratteri ed overflow
-        if (!ft_atoi_save(argv[i], &value))
-        {
-            write(2, "Error\n", 6);
-            free_stack(stack_a);
-            exit(1);
-        }
-        // 2. Controlla i duplicati
-        if (has_duplicate(stack_a, value))
-        {
-            write(2, "Error\n", 6);
-            free_stack(stack_a);
-            exit(1);
-        }
-        // 3. Inserisci nello stack
-        if (!push_back(stack_a, value))
-        {
-            write(2, "Error\n", 6);
-            free_stack(stack_a);
-            exit(1);
-        }
-        i++;
-    }
-    return (1);
+	new_node = malloc(sizeof(t_node));
+	if (!new_node)
+		return (0);
+	new_node->value = value;
+	new_node->index = -1;
+	new_node->next = NULL;
+	if (!stack_a->top)
+	{
+		new_node->prev = NULL;
+		stack_a->top = new_node;
+	}
+	else
+	{
+		last = stack_a->top;
+		while (last->next)
+			last = last->next;
+		last->next = new_node;
+		new_node->prev = last;
+	}
+	stack_a->size++;
+	return (1);
+}
+
+/**
+ * Riconosce i flag dei selettori di strategia definiti dal soggetto.
+ * Utilizza la tua ft_strncmp della libft per i controlli.
+ */
+int	strategy_selector(char *s, int *strategy)
+{
+	if (ft_strncmp(s, "--adaptive", 11) == 0)
+		*strategy = 0;
+	else if (ft_strncmp(s, "--simple", 9) == 0)
+		*strategy = 1;
+	else if (ft_strncmp(s, "--medium", 9) == 0)
+		*strategy = 2;
+	else if (ft_strncmp(s, "--complex", 10) == 0)
+		*strategy = 3;
+	else
+		return (0);
+	return (1);
+}
+
+/**
+ * Funzione principale per il parsing. Legge l'array degli argomenti,
+ * estrae le sotto-stringhe tramite ft_split e popola in modo sicuro lo stack.
+ */
+void	parse_arguments(int argc, char **argv, t_pushswap *ps, int *strategy)
+{
+	int		i;
+	int		j;
+	int		value;
+	char	**split_args;
+
+	i = 1;
+	*strategy = 0;
+	if (argc > 1 && strategy_selector(argv[1], strategy))
+		i++;
+	while (i < argc)
+	{
+		split_args = ft_split(argv[i], ' ');
+		if (!split_args)
+			handle_parse_error(ps, NULL);
+		j = 0;
+		// Se la stringa passata era vuota (es. ""), split non mette elementi e saltiamo il blocco
+		while (split_args[j])
+		{
+			if (!ft_atoi_save(split_args[j], &value)
+				|| has_duplicate(&ps->a, value)
+				|| !push_back(&ps->a, value))
+				handle_parse_error(ps, split_args);
+			j++;
+		}
+		free_matrix(split_args);
+		i++;
+	}
 }
