803b4a00abddd48a9011a2b3fe997699625aa002
MarcoCoppola135 <marcocoppola135@gmail.com>
2026-06-24 15:49:31 +0200

    Create push_operations.c
    operazioni di PUSH

---
+++ push_operations.c
@@ -0,0 +1,43 @@
+#include "push_swap.h"
+
+// Logica per spostare il nodo dallo stack src allo tsack dest
+
+static void	push(t_stack *dest, t_stack *src)
+{
+	t_node	*node_to_move;
+
+	if (!src || src->size == 0 || !src->top)
+		return ;
+	//1. Sgancio il nodo dallo stack sorgente
+	node_to_move = src->top;
+	src->top = node_to_move->next;
+	if (src->top)
+		src->top->prev = NULL;
+	src->size--;
+	//2. Inserisco il nodo appena sganciato in cima allo stack destinazione
+	node_to_move->next = dest->top;
+	if (dest->top)
+		dest->top->prev = node_to_move;
+	dest->top = node_to_move;
+	dest->size++;
+}
+
+//pa (push a) prende il primo elemento in cima a b e lo mette in cima ad a
+//Non fa nulla se b è vuoto
+
+void	pa(t_pushswap *ps)
+{
+	push(&ps->a, &ps->b);
+	write(1, "pa\n", 3);
+	ps->op_count++;
+}
+
+//pb (push b) prende il primo elemento in cima ad a e lo mette in cima a b
+//Non fa nulla se a è vuoto
+
+void	pb(t_pushswap *ps)
+{
+	push(&ps->b, &ps->a);
+	write(1, "pb\n", 3);
+	ps->op_count++;
+}
