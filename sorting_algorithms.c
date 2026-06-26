3084801def4aab2d7dbb10590fdb0b9a1132b722
MarcoCoppola135 <marcocoppola135@gmail.com>
2026-06-26 16:52:36 +0200

    Update sorting_algorithms.c

--- sorting_algorithms.c
+++ sorting_algorithms.c
@@ -37,6 +37,76 @@ static void	set_size(int *size, t_pushswap *ps)
 	size[1] = (&ps->b)->size;
 }
 
+/**
+ * Trova la posizione (indice di nodo) del valore cercato nello stack B.
+ * Serve per calcolare se conviene fare rb o rrb.
+ */
+static int	get_pos_in_b(t_stack *b, int target_index)
+{
+	t_node	*curr;
+	int		pos;
+
+	curr = b->top;
+	pos = 0;
+	while (curr)
+	{
+		if (curr->index == target_index)
+			return (pos);
+		pos++;
+		curr = curr->next;
+	}
+	return (0);
+}
+
+/**
+ * Algoritmo Medio: Chunk/Sandglass Sort. Complessità O(n rad(n)).
+ * Spinge i nodi in B divisi per blocchi dinamici e li riprende in modo ottimizzato.
+ */
+void	medium_sort(t_pushswap *ps)
+{
+	int	i;
+	int	chunk_size;
+
+	i = 0;
+	// Dimensione ottimale del chunk approssimata a √n
+	chunk_size = 1;
+	while (chunk_size * chunk_size < ps->a.size)
+		chunk_size++;
+	chunk_size *= 2; // Moltiplicatore empirico per ottimizzare il numero di mosse
+	// Fase 1: Spinta condizionata in B
+	while (ps->a.size > 0)
+	{
+		if (ps->a.top->index <= i)
+		{
+			pb(ps, 1);
+			rb(ps, 1); // Ruota in fondo i più piccoli per creare la clessidra
+			i++;
+		}
+		else if (ps->a.top->index <= i + chunk_size)
+		{
+			pb(ps, 1);
+			i++;
+		}
+		else
+			ra(ps, 1);
+	}
+	// Fase 2: Svuotamento controllato di B verso A
+	while (ps->b.size > 0)
+	{
+		if (get_pos_in_b(&ps->b, ps->b.size - 1) < ps->b.size / 2)
+		{
+			while (ps->b.top->index != ps->b.size - 1)
+				rb(ps, 1);
+		}
+		else
+		{
+			while (ps->b.top->index != ps->b.size - 1)
+				rrb(ps, 1);
+		}
+		pa(ps, 1);
+	}
+}
+
 void	radix_sort(t_pushswap *ps)
 {
 	int	bucket;
