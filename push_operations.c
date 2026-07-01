ecc7f310eac0b92825ea59f040067c2e91cd1ce8
MarcoCoppola135 <marcocoppola135@gmail.com>
2026-07-01 11:17:25 +0200

    Update push_operations.c

--- push_operations.c
+++ push_operations.c
@@ -1,6 +1,16 @@
-#include "push_swap.h"
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push_operations.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: marcoppo <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/07/01 10:12:37 by marcoppo          #+#    #+#             */
+/*   Updated: 2026/07/01 10:12:40 by marcoppo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
-// Logica per spostare il nodo dallo stack src allo tsack dest
+#include "push_swap.h"
 
 static void	push(t_stack *dest, t_stack *src)
 {
@@ -8,13 +18,11 @@ static void	push(t_stack *dest, t_stack *src)
 
 	if (!src || src->size == 0 || !src->top)
 		return ;
-	//1. Sgancio il nodo dallo stack sorgente
 	node_to_move = src->top;
 	src->top = node_to_move->next;
 	if (src->top)
 		src->top->prev = NULL;
 	src->size--;
-	//2. Inserisco il nodo appena sganciato in cima allo stack destinazione
 	node_to_move->next = dest->top;
 	if (dest->top)
 		dest->top->prev = node_to_move;
@@ -22,14 +30,11 @@ static void	push(t_stack *dest, t_stack *src)
 	dest->size++;
 }
 
-//pa (push a) prende il primo elemento in cima a b e lo mette in cima ad a
-//Non fa nulla se b è vuoto
-
 void	pa(t_pushswap *ps, int bench)
 {
 	static int	count = 0;
 
-	if(bench == 0)
+	if (bench == 0)
 	{
 		ft_putstr_fd(" pa: ", 2);
 		ft_putnbr_fd(count, 2);
@@ -42,14 +47,11 @@ void	pa(t_pushswap *ps, int bench)
 	ps->op_count++;
 }
 
-//pb (push b) prende il primo elemento in cima ad a e lo mette in cima a b
-//Non fa nulla se a è vuoto
-
 void	pb(t_pushswap *ps, int bench)
 {
 	static int	count = 0;
 
-	if(bench == 0)
+	if (bench == 0)
 	{
 		ft_putstr_fd(" pb: ", 2);
 		ft_putnbr_fd(count, 2);
