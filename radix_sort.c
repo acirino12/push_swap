736877c21e46b5ee66ccd99b854e6cd22d78fb55
MarcoCoppola135 <marcocoppola135@gmail.com>
2026-07-01 11:21:28 +0200

    Update radix_sort.c

--- radix_sort.c
+++ radix_sort.c
@@ -12,56 +12,56 @@
 
 #include "push_swap.h"
 
-static void process_a(t_pushswap *ps, int size_a, int bucket)
+static void	process_a(t_pushswap *ps, int size_a, int bucket)
 {
-    while (size_a--)
-    {
-        if ((ps->a.top->index & bucket) == 0)
-            ra(ps, 1);
-        else
-            pb(ps, 1);
-    }
+	while (size_a--)
+	{
+		if ((ps->a.top->index & bucket) == 0)
+			ra(ps, 1);
+		else
+			pb(ps, 1);
+	}
 }
 
-static void process_b(t_pushswap *ps, int size_b, int bucket)
+static void	process_b(t_pushswap *ps, int size_b, int bucket)
 {
-    while (size_b--)
-    {
-        rrb(ps, 1);
-        if ((ps->b.top->index & bucket) == 0)
-        {
-            pa(ps, 1);
-            ra(ps, 1);
-        }
-    }
+	while (size_b--)
+	{
+		rrb(ps, 1);
+		if ((ps->b.top->index & bucket) == 0)
+		{
+			pa(ps, 1);
+			ra(ps, 1);
+		}
+	}
 }
 
-static void set_size(int *size, t_pushswap *ps)
+static void	set_size(int *size, t_pushswap *ps)
 {
-    size[0] = (&ps->a)->size;
-    size[1] = (&ps->b)->size;
+	size[0] = (&ps->a)->size;
+	size[1] = (&ps->b)->size;
 }
 
-void    radix_sort(t_pushswap *ps)
+void	radix_sort(t_pushswap *ps)
 {
-    int bucket;
-    int size[2];
+	int	bucket;
+	int	size[2];
 
-    if (compute_disorder(&ps->a) == 0)
-        return ;
-    bucket = 1;
+	if (compute_disorder(&ps->a) == 0)
+		return ;
+	bucket = 1;
 	set_size(size, ps);
-    while (bucket < size[0] + size[1])
-    {
-        process_a(ps, size[0], bucket);
-        process_b(ps, size[1], bucket);
-        bucket *= 2;
+	while (bucket < size[0] + size[1])
+	{
+		process_a(ps, size[0], bucket);
+		process_b(ps, size[1], bucket);
+		bucket *= 2;
 		set_size(size, ps);
-    }
-    while (size[1]--)
-    {
-        rrb(ps, 1);
-        pa(ps, 1);
-        ra(ps, 1);
-    }
+	}
+	while (size[1]--)
+	{
+		rrb(ps, 1);
+		pa(ps, 1);
+		ra(ps, 1);
+	}
 }
