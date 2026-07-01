996121bf9219d59e3bccd631f4c20b1d15628d3d
MarcoCoppola135 <marcocoppola135@gmail.com>
2026-07-01 11:16:55 +0200

    Update medium_sort.c

--- medium_sort.c
+++ medium_sort.c
@@ -12,50 +12,50 @@
 
 #include "push_swap.h"
 
-static int  calculate_chunk(int size)
+static int	calculate_chunk(int size)
 {
-    int chunk;
+	int	chunk;
 
-    chunk = 1;
-    while (chunk * chunk < size)
-        chunk++;
-    return (chunk * 2);
+	chunk = 1;
+	while (chunk * chunk < size)
+		chunk++;
+	return (chunk * 2);
 }
 
-static void empty_b_to_a(t_pushswap *ps)
+static void	empty_b_to_a(t_pushswap *ps)
 {
-    while (ps->b.size > 0)
-    {
-        if (get_position(ps->b.top, ps->b.size - 1) < ps->b.size / 2)
-            while (ps->b.top->index != ps->b.size - 1)
-                rb(ps, 1);
-        else
-            while (ps->b.top->index != ps->b.size - 1)
-                rrb(ps, 1);
-        pa(ps, 1);
-    }
+	while (ps->b.size > 0)
+	{
+		if (get_position(ps->b.top, ps->b.size - 1) < ps->b.size / 2)
+			while (ps->b.top->index != ps->b.size - 1)
+				rb(ps, 1);
+		else
+			while (ps->b.top->index != ps->b.size - 1)
+				rrb(ps, 1);
+		pa(ps, 1);
+	}
 }
 
-void    medium_sort(t_pushswap *ps)
+void	medium_sort(t_pushswap *ps)
 {
-    int i;
-    int chunk_size;
+	int	i;
+	int	chunk_size;
 
-    if (compute_disorder(&ps->a) == 0)
-        return ;
-    i = 0;
-    chunk_size = calculate_chunk(ps->a.size);
-    while (ps->a.size > 0)
-    {
-        if (ps->a.top->index <= i + chunk_size)
-        {
-            pb(ps, 1);
-            if (ps->b.top->index <= i)
-                rb(ps, 1);
-            i++;
-        }
-        else
-            ra(ps, 1);
-    }
-    empty_b_to_a(ps);
+	if (compute_disorder(&ps->a) == 0)
+		return ;
+	i = 0;
+	chunk_size = calculate_chunk(ps->a.size);
+	while (ps->a.size > 0)
+	{
+		if (ps->a.top->index <= i + chunk_size)
+		{
+			pb(ps, 1);
+			if (ps->b.top->index <= i)
+				rb(ps, 1);
+			i++;
+		}
+		else
+			ra(ps, 1);
+	}
+	empty_b_to_a(ps);
 }
