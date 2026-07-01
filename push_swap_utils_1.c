e5264ed1c955513a8e7115a17aa7b195c4a10ecb
MarcoCoppola135 <marcocoppola135@gmail.com>
2026-07-01 11:20:29 +0200

    Update push_swap_utils_1.c

--- push_swap_utils_1.c
+++ push_swap_utils_1.c
@@ -10,10 +10,9 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-
 #include "push_swap.h"
 
-int ft_atoi_save(const char *str, int *result)
+int	ft_atoi_save(const char *str, int *result)
 {
 	long	num;
 	int		sign;
@@ -30,7 +29,7 @@ int ft_atoi_save(const char *str, int *result)
 	while (*str >= '0' && *str <= '9')
 	{
 		num = num * 10 + (*str++ - '0');
-		if ((sign == 1 && num > INT_MAX) || (sign == -1 && -num < INT_MIN))
+		if ((sign == 1 && num > INT_MAX) || (sign == -1 && - num < INT_MIN))
 			return (0);
 	}
 	if (*str != '\0')
@@ -39,68 +38,57 @@ int ft_atoi_save(const char *str, int *result)
 	return (1);
 }
 
-void assign_indexes(t_stack *stack_a)
+void	assign_indexes(t_stack *stack_a)
 {
-    t_node  *current;
-    t_node  *compare;
-    int     final_position;
+	t_node	*current;
+	t_node	*compare;
+	int		final_position;
 
-    current = stack_a->top;
-    while (current != NULL)
-    {
-        final_position = 0;
-        compare = stack_a->top;
-        
-        // Confrontiamo il nodo attuale con tutti gli altri nodi dello stack
-        while (compare != NULL)
-        {
-            // Se troviamo un numero più piccolo del nostro, 
-            // significa che il nostro numero dovrà stare dopo di lui
-            if (current->value > compare->value)
-                final_position++;
-            compare = compare->next;
-        }
-        
-        // Assegniamo la posizione finale calcolata come indice del nodo
-        current->index = final_position;
-        current = current->next;
-    }
+	current = stack_a->top;
+	while (current != NULL)
+	{
+		final_position = 0;
+		compare = stack_a->top;
+		while (compare != NULL)
+		{
+			if (current->value > compare->value)
+				final_position++;
+			compare = compare->next;
+		}
+		current->index = final_position;
+		current = current->next;
+	}
 }
 
-int compute_disorder(t_stack *stack_a)
+int	compute_disorder(t_stack *stack_a)
 {
-    t_node  *i;
-    t_node  *j;
-    int     mistakes;
-    int     total_pairs;
+	t_node	*i;
+	t_node	*j;
+	int		mistakes;
+	int		total_pairs;
 
-    mistakes = 0;
-    total_pairs = 0;
-    if (!stack_a || stack_a->size < 2)
-        return (0);
-    i = stack_a->top;
-    while (i != NULL)
-    {
-        j = i->next;
-        while (j != NULL)
-        {
-            total_pairs += 1;
-            if (i->index > j->index)
-                mistakes += 1;
-            j = j->next;
-        }
-        i = i->next;
-    }
-    if (total_pairs == 0)
-        return (0);
-    return ((int)(((long)mistakes * 10000) / total_pairs));
+	mistakes = 0;
+	total_pairs = 0;
+	if (!stack_a || stack_a->size < 2)
+		return (0);
+	i = stack_a->top;
+	while (i != NULL)
+	{
+		j = i->next;
+		while (j != NULL)
+		{
+			total_pairs += 1;
+			if (i->index > j->index)
+				mistakes += 1;
+			j = j->next;
+		}
+		i = i->next;
+	}
+	if (total_pairs == 0)
+		return (0);
+	return ((int)(((long)mistakes * 10000) / total_pairs));
 }
 
-
-/**
- * Gestisce l'errore di parsing stampando "Error" su stderr,
- * liberando la memoria attiva e terminando il programma.
- */
 void	handle_parse_error(t_pushswap *ps, char **matrix)
 {
 	write(2, "Error\n", 6);
